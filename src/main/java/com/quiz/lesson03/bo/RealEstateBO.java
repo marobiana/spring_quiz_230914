218080500a0da67e71a0afb1a46a4970351827e5
DESKTOP-B0NSHI7\user <test@test.com>
2023-12-19 18:43:21 +0900

    insert 문제 풀이

--- src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -23,6 +23,25 @@ public class RealEstateBO {
 	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice) {
 		return realEstateMapper.selectRealEstateListByRentPrice(rentPrice);
 	}
+	
+	// input:area, price     output:List<RealEstate> [] or 채워져있는 리스트
+	public List<RealEstate> getRealEstateListByAreaPrice(int area, int price) {
+		return realEstateMapper.selectRealEstateListByAreaPrice(area, price);
+	}
+	
+	// input:RealEstate      output: 성공한 행의 개수(int)
+	public int addRealEstate(RealEstate realEstate) {
+		return realEstateMapper.insertRealEstate(realEstate);
+	}
+	
+	//addRealEstateAsField(realtorId, "썅떼빌리버 오피스텔 814호", 45, "월세", 100000, 120)
+	public int addRealEstateAsField(
+			int realtorId, 
+			String address, int area, String type,
+			int price, Integer rentPrice) {
+		
+		return realEstateMapper.insertRealEstateAsField(realtorId, address, area, type, price, rentPrice);
+	}
 }
 
 
--- src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -2,6 +2,7 @@ package com.quiz.lesson03.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson03.domain.RealEstate;
@@ -14,6 +15,25 @@ public interface RealEstateMapper {
 	
 	// input:rentPrice   output: List<RealEstate>([] or 채워져있거나)
 	public List<RealEstate> selectRealEstateListByRentPrice(int rentPrice);
+	
+	// input:area, price     output:List<RealEstate> [] or 채워져있는 리스트
+	public List<RealEstate> selectRealEstateListByAreaPrice(
+			// myBatis 문법 상 파라미터는 단 한개만 xml로 보낼 수 있다.
+			// 파라미터들을 하나의 맵에 담아서 보낸다.
+			// 맵으로 만들어주는 어노테이션 @Param
+			@Param("area") int area111111111, 
+			@Param("price") int price111111111); 
+	
+	// input:RealEstate      output: 성공한 행의 개수(int)
+	public int insertRealEstate(RealEstate realEstate);
+	
+	public int insertRealEstateAsField(
+			@Param("realtorId") int realtorId, 
+			@Param("address") String address, 
+			@Param("area") int area, 
+			@Param("type") String type,
+			@Param("price") int price, 
+			@Param("rentPrice") Integer rentPrice);
 }
 
 
