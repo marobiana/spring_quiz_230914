61efd61d0ba16d8f946ed2581db0c3b7b9acf306
DESKTOP-B0NSHI7\user <test@test.com>
2023-12-21 18:45:33 +0900

    JSP 문제1 풀이

---
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -0,0 +1,71 @@
+package com.quiz.lesson04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.domain.Seller;
+
+@RequestMapping("/lesson04/quiz01")
+@Controller
+public class Lesson04Quiz01Controller {
+	
+	@Autowired
+	private SellerBO sellerBO;
+
+	// 판매자 추가 화면
+	// http://localhost:8080/lesson04/quiz01/add-seller-view
+	@GetMapping("/add-seller-view")
+	public String addSellerView() {
+		return "lesson04/addSeller";
+	}
+	
+	// DB insert => 입력 성공 화면
+	@PostMapping("/add-seller")
+	public String addSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value = "profileImageUrl", required = false) String profileImageUrl,
+			@RequestParam("temperature") double temperature) {
+		
+		// db insert
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		return "lesson04/afterAddSeller";
+	}
+	
+	// 최근 가입자 한명 화면
+	// http://localhost:8080/lesson04/quiz01/seller-info-view
+	// http://localhost:8080/lesson04/quiz01/seller-info-view?id=2
+	@GetMapping("/seller-info-view")
+	public String sellerInfoView(
+			@RequestParam(value = "id", required = false) Integer id,
+			Model model) {
+		// db select
+		Seller seller = null;
+		if (id == null) {
+			seller = sellerBO.getLatestSeller();
+		} else {
+			seller = sellerBO.getSellerById(id);
+		}
+		
+		// model에 담기
+		model.addAttribute("seller", seller);
+		model.addAttribute("title", "판매자 정보");
+		
+		// 응답 화면
+		return "lesson04/sellerInfo";
+	}
+}
+
+
+
+
+
+
+
+
