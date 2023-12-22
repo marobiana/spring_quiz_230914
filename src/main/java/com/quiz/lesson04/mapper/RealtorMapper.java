daa4f6246abba6814b8fadbc3b4ec57e4a1a559e
DESKTOP-B0NSHI7\user <test@test.com>
2023-12-22 16:59:43 +0900

    4강 문제 2번

---
+++ src/main/java/com/quiz/lesson04/mapper/RealtorMapper.java
@@ -0,0 +1,18 @@
+package com.quiz.lesson04.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.quiz.lesson04.domain.Realtor;
+
+@Repository
+public interface RealtorMapper {
+
+	public int insertRealtor(Realtor realtor);
+	
+	public Realtor selectRealtorById(int id);
+}
+
+
+
+
+
